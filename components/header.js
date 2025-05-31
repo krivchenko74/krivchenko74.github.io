a7abb9884af136dc5cbde512d0fe59b7f54c9750
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-05-31 09:06:07 +0500

    added header markdown

---
+++ components/header.js
@@ -0,0 +1,35 @@
+const body = document.querySelector("body");
+
+if (!body) console.error("No body element found!");
+
+const links = [
+  { href: "./index.html", text: "Главная" },
+  { href: "./blog.html", text: "Блог" },
+];
+
+const header = document.createElement("header");
+const img = document.createElement("img");
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+
+header.classList.add("header");
+img.classList.add("logo");
+img.src = "./img/logo.svg";
+img.alt = "Логотип сайта";
+nav.classList.add("nav");
+ul.classList.add("nav-list");
+
+links.forEach((link) => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = link.href;
+  a.textContent = link.text;
+  li.appendChild(a);
+  ul.appendChild(li);
+});
+
+nav.appendChild(ul);
+header.appendChild(img);
+header.appendChild(nav);
+
+body.insertBefore(header, body.firstChild);
